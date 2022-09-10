bb05b53eec8f7f4eca9d4d96b9427380f7fca5fe
Saransh Dabas <62711371+groodking@users.noreply.github.com>
2022-09-10 20:02:36 +0530

    LEVEL 1 completed(Algorithm wise)
    Solving errors

--- chess.cpp
+++ chess.cpp
@@ -2,113 +2,166 @@
 #include <string.h>
 using namespace std;
 
-
-
-int move checker(string player_name,char playerAinputs[5], char playerBinputs[5],string character_name, string move_name, char chess_grid[5][5])
+int move_checker(string player_name,char playerAinputs[5], char playerBinputs[5],string character_name, string move_name, char chess_grid[5][5])
 {
-	if(move_name == "L")
+	for(int i=0;i<5;i++)
 	{	
-		for(int i=0;i<5;i++)
+		for(int j=0;j<5;j++)
 		{	
-			for(int j=0;j<5;j++)
+			if(chess_grid[i][j] == player_name+"-"+character_name)
+			{	
+				// Valid player check
+				if(!playerAinputs.contains(character_name) || !playerBinputs.contains(character_name) ){
+					return -1;
+				}
+				// Valid move check
+				else if(move_name != "L" && move_name != "B" && move_name != "F" && move_name != "R"){
+					return -1;
+				}
+
+				// boundary check
+				else if(i+1 > 5 || j+1 > 5 || i-1 < 0 || j-1 < 0){
+					return -1;
+				}
 				
-				if(chess_grid[i][j] == player_name+"-"+character_name)
-				{	
+				if(player_name == "A")
+				{
+					
+					// friendly fire check
 					if(chess_grid[i][j-1].contains("A")){
-						cout<<"Invalid move, try again!";
 						return -1;
 					}
-
-					else if(!playe.contains()){
-						cout<<"invalid move, try again!";
+					else if(chess_grid[i][j+1].contains("A")){
+						return -1;
+					}
+					else if(chess_grid[i+1][j].contains("A")){
+						return -1;
+					}
+					else if(chess_grid[i-1][j].contains("A")){
 						return -1;
 					}
 				}
+				else if(player_name == "B")
+				{
+					
+					// friendly fire check
+					if(chess_grid[i][j-1].contains("B")){
+						return -1;
+					}
+					else if(chess_grid[i][j+1].contains("B")){
+						return -1;
+					}
+					else if(chess_grid[i+1][j].contains("B")){
+						return -1;
+					}
+					else if(chess_grid[i-1][j].contains("B")){
+						return -1;
+					}
+				}
+			}
 		}
 	}
-	
-		else if(move_name == "R")
-		{	
-			for(int i=0;i<5;i++)
-			{	cout<<"\n";
-				for(int j=0;j<5;j++)
-					chess_grid[i][j];
-			}
-		}
-		else if(move_name == "F")
-		{	
-			for(int i=0;i<5;i++)
-			{	cout<<"\n";
-				for(int j=0;j<5;j++)
-					chess_grid[i][j];
-			}
-		}
-		else if(move_name == "B")
-		{	
-			for(int i=0;i<5;i++)
-			{	cout<<"\n";
-				for(int j=0;j<5;j++)
-					chess_grid[i][j];
-			}
-		}
-
-		cout<<"Current Grid:"<<endl;
-		for(int i=0;i<5;i++)
-		{	cout<<"\n";
-			for(int j=0;j<5;j++)
-				chess_grid[i][j];
-		}
-	
 }
 
-void move_maker(string player_name,char playerAinputs[5], char playerBinputs[5],string character_name, string move_name, char chess_grid[5][5])
+void move_maker(string player_name,char playerAinputs[5], char playerBinputs[5],string character_name, string move_name, char chess_grid[5][5], int counter)
 {
-	if(move_name == "L")
+	for(int i=0;i<5;i++)
 	{	
-		for(int i=0;i<5;i++)
-		{	
-			for(int j=0;j<5;j++)
+		for(int j=0;j<5;j++)
+		{
+			if(chess_grid[i][j] == player_name+"-"+character_name)
+			{	
 				
-				if(chess_grid[i][j] == player_name+"-"+character_name)
-				{	
-					chess_grid[i][j-1] = chess_grid[i][j];
-					chess_grid[i][j] = "_";
+				if(move_name == "L")
+				{
+					if(player_name=="A")
+					{
+						if(chess_grid[i][j-1].contains("B"))
+							playerBinputs[counter] = '\0';  // making random inputs of player B as NULL, to keep track of how many players are left. 
 
-					if(chess_grid[i][j-1] ) 
+						chess_grid[i][j-1] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}
+
+					else if(player_name=="B")
+					{
+						if(chess_grid[i][j+1].contains("A"))
+							playerAinputs[counter] = '\0';   // making random inputs of player A as NULL, to keep track of how many players are left. 
+							
+						chess_grid[i][j+1] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}
+					
 				}
+				else if(move_name == "R")
+				{
+					if(player_name=="A")
+					{
+						if(chess_grid[i][j+1].contains("B"))
+							playerBinputs[counter] = '\0';  
+
+						chess_grid[i][j+1] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}
+
+					else if(player_name=="B")
+					{
+						if(chess_grid[i][j-1].contains("A"))
+							playerAinputs[counter] = '\0';  
+							
+						chess_grid[i][j-1] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}
+				}
+				else if(move_name == "F")
+				{
+					if(player_name=="A")
+					{
+						if(chess_grid[i-1][j].contains("B"))
+							playerBinputs[counter] = '\0';  
+
+						chess_grid[i-1][j] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}
+
+					else if(player_name=="B")
+					{
+						if(chess_grid[i+1][j].contains("A"))
+							playerAinputs[counter] = '\0';  
+							
+						chess_grid[i+1][j] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}
+				}
+				else if(move_name == "B")
+				{
+					if(player_name=="A")
+					{
+						if(chess_grid[i+1][j].contains("B"))
+							playerBinputs[counter] = '\0';  
+
+						chess_grid[i+1][j] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}
+
+					else if(player_name=="B")
+					{
+						if(chess_grid[i-1][j].contains("A"))
+							playerAinputs[counter] = '\0';  
+							
+						chess_grid[i-1][j] = chess_grid[i][j];
+						chess_grid[i][j] = "_";
+					}  
+				}
+			}
 		}
 	}
-	
-		else if(move_name == "R")
-		{	
-			for(int i=0;i<5;i++)
-			{	cout<<"\n";
-				for(int j=0;j<5;j++)
-					chess_grid[i][j];
-			}
-		}
-		else if(move_name == "F")
-		{	
-			for(int i=0;i<5;i++)
-			{	cout<<"\n";
-				for(int j=0;j<5;j++)
-					chess_grid[i][j];
-			}
-		}
-		else if(move_name == "B")
-		{	
-			for(int i=0;i<5;i++)
-			{	cout<<"\n";
-				for(int j=0;j<5;j++)
-					chess_grid[i][j];
-			}
-		}
 
-		cout<<"Current Grid:"<<endl;
+	cout<<"Current Grid:"<<endl;
 		for(int i=0;i<5;i++)
 		{	cout<<"\n";
 			for(int j=0;j<5;j++)
-				chess_grid[i][j];
+				cout<< chess_grid[i][j] << " ";
 		}
 }
 
@@ -121,9 +174,9 @@ int main()
 	char ipA[5], ipB[5];
 	string moves, character_name, move_name;
 
-	int result; // whether the move is valid or not
+	int result, pos, counterA = 0, counterB = 0; // for personal refernce to keep track of deleted elements; // whether the move is valid or not
 
-	char chess_grid[5][5];
+	string chess_grid[5][5];
 
 	// initialize the game board
 	for(int i=0;i<5;i++)
@@ -138,7 +191,7 @@ int main()
 	
 	// inserting characters in grid
 	for(int i=0;i<5;i++)
-		chess_grid[4][i] = "A-"+ipA[i];
+		chess_grid[4][i] = "A"+"-"+ipA[i];
 	
 
 	// Displaying the current board
@@ -158,7 +211,7 @@ int main()
 	
 	// inserting characters in grid
 	for(int i=0;i<5;i++)
-		chess_grid[0][i] = "B-"+ipB[i];
+		chess_grid[0][i] = "B"+"-"+ipB[i];
 	
 
 	// Displaying the current board
@@ -173,35 +226,36 @@ int main()
 
 	while(strlen(ipA) == 0 || strlen(ipB) == 0)
 	{
-
+			
 			cout << "Player A's Move: ";
 			// taking input characters
 			for(int i=0;i<5;i++)
 				cin >> moves;
 			
 			// fidning the entere move and chess piece
-			int pos = moves.find(":");
+			pos = moves.find(":");
 			character_name = moves.substr(0,pos - 1);
 			move_name = moves.substr(pos + 1);
 
-			int result = move_checker("A",ipA,ipB,character_name,move_name,chess_grid);//to check for invalid moves
+			result = move_checker("A",ipA,ipB,character_name,move_name,chess_grid);//to check for invalid moves
 			
 			while(result == -1)
 			{
-				cout << "Invalid Move, Try again!!"
+				cout << "Invalid Move, Try again!!";
 				cout << "Player A's Move: ";
 				for(int i=0;i<5;i++)
 					cin >> moves;
 
-				int pos = moves.find(":");
+				pos = moves.find(":");
 				character_name = moves.substr(0,pos - 1);
 				move_name = moves.substr(pos + 1);
 
 				result = move_checker("A",ipA,ipB,character_name,move_name,chess_grid);
 			}
 			
+			counterA++;
+			move_maker("A",ipA,ipB,character_name,move_name,chess_grid,counterA); // to make the changes in grid and displaying the result 
 
-			move_maker("A",ipA,ipB,character_name,move_name,chess_grid); // to make the changes in grid and displaying the result 
 
 		////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			cout << "Player B's Move: ";
@@ -209,27 +263,27 @@ int main()
 			for(int i=0;i<5;i++)
 				cin >> moves;
 
-			int pos = moves.find(":");
+			pos = moves.find(":");
 			character_name = moves.substr(0,pos - 1);
 			move_name = moves.substr(pos + 1);
 
-			int result = move_checker("B",ipA,ipB,character_name,move_name,chess_grid);//to check for invalid moves
+			result = move_checker("B",ipA,ipB,character_name,move_name,chess_grid);//to check for invalid moves
 			
 			while(result == -1)
 			{
-				cout << "Invalid Move, Try again!!"
+				cout << "Invalid Move, Try again!!";
 				cout << "Player B's Move: ";
 				for(int i=0;i<5;i++)
 					cin >> moves;
 
-				int pos = moves.find(":");
+				pos = moves.find(":");
 				character_name = moves.substr(0,pos - 1);
 				move_name = moves.substr(pos + 1);
 
 				result = move_checker("B",ipA,ipB,character_name,move_name,chess_grid);
 			}
-
-			move_maker("B",ipA,ipB,character_name,move_name,chess_grid); // to make the changes in grid and displaying the result
+			counterB++;
+			move_maker("B",ipA,ipB,character_name,move_name,chess_grid,counterB); // to make the changes in grid and displaying the result
 	}
 
 	if(strlen(ipA) == 0)
